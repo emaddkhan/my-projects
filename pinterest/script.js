52a7c730415d790e83e2f3ca1a9a05410468b17e
My Name <emadkhan.yousufzai@gmail.com>
2025-03-23 23:44:55 +0500

    pinterest project

--- pinterest/script.js
+++ pinterest/script.js
@@ -1,3 +1,7 @@
+const imgContainer = document.querySelector('.container');
+const overlay = document.querySelector('.overlay');
+const searchInput = document.querySelector('#searchinput');
+
 var arr = [
     {name: "Petals of roses", image: "https://images.unsplash.com/photo-1518621736915-f3b1c41bfd00?q=80&w=3786&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"},
     {name: "Animals of town", image: "https://images.unsplash.com/photo-1583337130417-3346a1be7dee?q=80&w=2688&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" },
@@ -16,39 +20,20 @@ function showTheCards(){
         <div class="caption">Lorem ipsum </div>
     </div>`;
     })
-
-    document.querySelector(".container")
-    .innerHTML = clutter;
+    imgContainer.innerHTML=clutter
 }
 
-function handleSearchFunctionality(){
-    var input = document.querySelector("#searchinput");
-    
-
-    input
-    .addEventListener("focus", function(){
-        document.querySelector(".overlay").style.display = "block";
+function searchFunctionHandler(){
+    searchInput.addEventListener("focus",()=>{
+        overlay.style.display ="block";
     })
-
-    input
-    .addEventListener("blur", function(){
-        document.querySelector(".overlay").style.display = "none";
+    searchInput.addEventListener("blur",()=>{
+        overlay.style.display ="none";
     })
-
-    input
-    .addEventListener("input", function(){
-        const filteredArray = arr.filter(obj => obj.name.toLowerCase().startsWith(input.value));
-        var clutter = "";
-        filteredArray.forEach(function(obj){
-            clutter += `<div class="res flex px-8 py-3">
-            <i class="ri-search-line font-semibold mr-5"></i>
-            <h3 class="font-semibold">${obj.name}</h3>
-        </div>`
-        })
-    document.querySelector(".searchdata").style.display = "block";
-        document.querySelector(".searchdata").innerHTML = clutter;
+    searchInput.addEventListener("input",()=>{
+        const filteredArray = arr.filter(obj=>obj.name.toLowerCase().startsWith(searchInput.value));
+        console.log(filteredArray)
     })
 }
-
-handleSearchFunctionality();
+searchFunctionHandler();
 showTheCards();
