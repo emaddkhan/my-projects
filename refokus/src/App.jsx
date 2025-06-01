a92085901a955dddd97d94636a69cb2c186020ed
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-06-01 22:25:17 +0500

    working on refokus

--- refokus/src/App.jsx
+++ refokus/src/App.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Work from './components/Work'
 
 function App() {
   return (
     <div className='h-screen w-full bg-[#1C1C1C] text-white'>
       <Navbar/>
+      <Work/>
     </div>
   )
 }
---
+++ refokus/src/components/Work.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Work() {
+  return (
+    <div className='w-full'>
+        <div className='max-w-screen-xl text-center  mx-auto'>
+            <h1 className='text-[30vw] select-none leading-none font-medium tracking-tight '>work</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Work
