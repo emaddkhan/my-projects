b63c681e62f1947a3ee601cba3019f4a95fb090b
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-06-05 23:34:20 +0500

    products animation of Refokus completed

--- refokus/src/components/Products.jsx
+++ refokus/src/components/Products.jsx
@@ -2,6 +2,11 @@ import React, { useState } from "react";
 import Product from "./Product";
 import { motion } from "motion/react";
 
+import arqitel from "../assets/videos/arqitel.mp4";
+import ttr from "../assets/videos/ttr.mp4";
+import yir from "../assets/videos/yir.mp4";
+import yahoo from "../assets/videos/yahoo.mp4";
+import rainfall from "../assets/videos/rainfall.mp4";
 function Products() {
   var products = [
     {
@@ -52,58 +57,63 @@ function Products() {
       })}
       <div className="absolute h-full w-full top-0  pointer-events-none">
         <motion.div
-          transition={{ ease: [0.76, 0, 0.24, 1],duration:.6 }}
+          transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.6 }}
           initial={{ y: pos, x: "-50%" }}
           animate={{ y: pos + "rem" }}
-          className="window  overflow-hidden -translate-x-[50%] h-[23rem] absolute w-[32rem] left-[44%]"
+          className="window rounded-lg  overflow-hidden -translate-x-[50%] h-[23rem] absolute w-[32rem] left-[44%]"
         >
           <motion.div
-            transition={{ ease: [0.76, 0, 0.24, 1],duration:.5 }}
+            transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
             animate={{ y: -pos + "rem" }}
             className="bg-zinc-200 h-full w-full "
           >
             {pos === 0 && (
-                            <video autoPlay muted loop>
-                                <source src={arqitel} type="video/webm" />
-                                Your browser does not support the video tag.
-                            </video>
-                        )}
+              <video autoPlay muted loop>
+                <source src={arqitel} type="video/webm" />
+                Your browser does not support the video tag.
+              </video>
+            )}
           </motion.div>
           <motion.div
-            transition={{ ease: [0.76, 0, 0.24, 1],duration:.5 }}
+            transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
             animate={{ y: -pos + "rem" }}
             className="bg-zinc-300 h-full w-full "
           >
             <video autoPlay muted loop>
-                            <source src={ttr} type="video/webm" />
-                            Your browser does not support the video tag.
-                        </video>
+              <source src={ttr} type="video/webm" />
+              Your browser does not support the video tag.
+            </video>
           </motion.div>
           <motion.div
-            transition={{ ease: [0.76, 0, 0.24, 1],duration:.5 }}
+            transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
             animate={{ y: -pos + "rem" }}
             className="bg-zinc-400 h-full w-full "
           >
-             <video autoPlay muted loop>
-                            <source src={yir} type="video/webm" />
-                            Your browser does not support the video tag.
-                        </video>
+            <video autoPlay muted loop>
+              <source src={yir} type="video/webm" />
+              Your browser does not support the video tag.
+            </video>
           </motion.div>
           <motion.div
-            transition={{ ease: [0.76, 0, 0.24, 1],duration:.5 }}
+            transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
             animate={{ y: -pos + "rem" }}
             className="bg-zinc-500 h-full w-full "
           >
             <video autoPlay muted loop>
-                            <source src={yahoo} type="video/webm" />
-                            Your browser does not support the video tag.
-                        </video>
+              <source src={yahoo} type="video/webm" />
+              Your browser does not support the video tag.
+            </video>
           </motion.div>
           <motion.div
-            transition={{ ease: [0.76, 0, 0.24, 1],duration:.5 }}
+            transition={{ ease: [0.76, 0, 0.24, 1], duration: 0.5 }}
             animate={{ y: -pos + "rem" }}
             className="bg-zinc-600 h-full w-full "
-          ></motion.div>
+          >
+            <video autoPlay muted loop>
+              <source src={rainfall} type="video/webm" />
+              Your browser does not support the video tag.
+            </video>
+          </motion.div>
         </motion.div>
       </div>
     </div>
