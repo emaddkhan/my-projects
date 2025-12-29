82dbe99c115df07cbd0246e9962ff3e643664728
emaddkhan <emadkhan.yousufzai@gmail.com>
2025-12-29 11:14:43 +0500

    making new project

--- k72/k72/src/Components/home/HomeHeroBottomText.jsx
+++ k72/k72/src/Components/home/HomeHeroBottomText.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom'
 function HomeHeroBottomText() {
   return (
     <div className='flex text-white items-center justify-center gap-10 font-[font2] '>
-      <Link to='/work' className='text-[6vw] leading-[6vw] flex items-end px-8 border-2 rounded-full uppercase'>Work</Link>
+      <Link to='/projects' className='text-[6vw] leading-[6vw] flex items-end px-8 border-2 rounded-full uppercase'>Work</Link>
       <Link to='/agency' className='text-[6vw] leading-[6vw] flex items-end px-8 border-2 rounded-full uppercase'>Agency </Link>
     </div>
   )
