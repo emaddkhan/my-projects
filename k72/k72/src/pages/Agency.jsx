9fc381a08a3d50f20f92ec86a7633e4781ac8efe
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-01-17 18:35:13 +0500

    making react project

--- k72/k72/src/pages/Agency.jsx
+++ k72/k72/src/pages/Agency.jsx
@@ -1,14 +1,21 @@
-import React from 'react'
+import React from "react";
 
 function Agency() {
   return (
-    <div className='font-[font2] text-center'>
-      <div className='mt-[55vh]'>
-        <h1 className='text-[20vw]  leading-[18vw] uppercase '>SEVEN7Y
-TWO</h1>
+    <div>
+      <div className="h-[20vw] w-[15vw] top-[10.9vw] left-[30.4vw] overflow-hidden rounded-3xl absolute">
+        <img src="https://k72.ca/images/teamMembers/Carl_480x640.jpg?w=480&h=640&fit=crop&s=f0a84706bc91a6f505e8ad35f520f0b7" alt="" />
+      </div>
+      <div className="font-[font2] text-center">
+      <div className="mt-[55vh]">
+        <h1 className="text-[20vw]  leading-[18vw] uppercase ">SEVEN7Y TWO</h1>
+      </div>
+      <div className="pl-[40%] mt-20">
+        <p className=" text-6xl text-start"> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;We’re inquisitive and open-minded, and we make sure creativity crowds out ego from every corner. A brand is a living thing, with values, a personality and a story. If we ignore that, we can achieve short-term success, but not influence that goes the distance. We bring that perspective to every brand story we help tell.</p>
       </div>
     </div>
-  )
+    </div>
+  );
 }
 
-export default Agency
+export default Agency;
