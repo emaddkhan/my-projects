c8c7ef1405e9028943c1504118da23cf3e9b88de
emaddkhan <emadkhan.yousufzai@gmail.com>
2026-04-19 12:39:11 +0500

    working on tmdb media section

--- tmdb/src/components/MovieDetailCastSection.jsx
+++ tmdb/src/components/MovieDetailCastSection.jsx
@@ -3,9 +3,10 @@ import MovieDetailSectionCastContainer from './MovieDetailSectionCastContainer'
 
 function MovieDetailCastSection({movie}) {
   return (
-    <div className='w-full h-[40vh] py-3 ' >
+    <div className='w-full h-[42vh] py-3   border-b-1 border-[#D7D7D7] ' >
         <h2 className='font-semibold text-xl'>Series Cast</h2>
         <MovieDetailSectionCastContainer movie={movie}/>
+        <h2 className='font-semibold underline text-[.9vw]  underline-offset-4 decoration-0 cursor-pointer hover:text-[#D7D7D7] transition-all duration-300 ease-in-out'>Full Cast & Crew</h2>
     </div>
   )
 }
